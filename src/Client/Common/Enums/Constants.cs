8ba1a3eb7b2d8bda7f886d89a14238b6498b05cb
Nathan Jekel <ijekel2@gmail.com>
2022-01-29 20:08:55 -0500

    Update styles for buttons and the main navigation bar.

---
+++ src/Client/Common/Enums/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterCraft.Client.Common.Enums
+{
+    public class Constants
+    {
+        public const string McGreen = "#228B22";
+        public const string McLightGreen = "#2cb42c";
+        public const string McBlack = "#1f1f1f";
+        public const string McLightBlack = "#3d3d3d";
+    }
+}
--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -27,6 +28,9 @@ namespace MasterCraft.Client
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddAuthorizationCore();
             builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+            builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTcyMzc1QDMxMzkyZTM0MmUzMG1nNi9ldkdHWWw3d3RTQ3lWbDJJN0FDTFY1eXNQOVBFaGxQRVFwa2E4Y289");
 
             await builder.Build().RunAsync();
         }
